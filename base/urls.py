2e97c4dac1ae7b3b1d3eb0ff994a558aa9e547fd
Jendrito <esteban.lavezzo@gmail.com>
2022-06-02 17:12:59 -0300

    agregue inicio

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from base.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contacto.urls')),
     path('', include('experiencias.urls')),
-    path('', include('tramites.urls'))
+    path('', include('tramites.urls')),
+    path('', inicio, name = 'inicio')
 ]
